9954f919c0c3d2966020afa40d2f79267b304a23
Natalie Perpepaj <perpepajn25@gmail.com>
2017-09-06 07:49:05 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :authors, except: [:destroy, :index]
+  resources :posts, except: [:destroy, :index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
